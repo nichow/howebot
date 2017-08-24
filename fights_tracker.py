e3e3ea8fae3906c001e130d777a24b2c3e3a2438
nichow <nhgarcia27@gmail.com>
2017-08-24 16:51:04 -0400

    fight tracker class
    first version of the class

---
+++ fights_tracker.py
@@ -0,0 +1,10 @@
+#fights_tracker.py
+
+import numpy as np
+from datetime import datetime.utcnow as utcnow
+
+class fights_tracker(self, game):
+  self.game = game
+  self.month = utcnow.month
+  self.ratings = []
+  self.avg = np.mean(np.array(ratings))
